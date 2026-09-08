43a95824422f6808ff7eb8c26cecc91d156de79b
BurdilesNicolas <burdilescarroandres.epet20@gmail.com>
2026-09-08 20:55:58 -0300

    Creando carpetas y archivos

---
+++ src/paginas/login.jsx
@@ -0,0 +1,15 @@
+function Login(){
+    return(
+        <div>
+            <h1>Inicio de seción</h1>
+            <form>
+                <label>Usuario</label>
+                <input type="text" placeholder="Username" />
+                <label>Contraseña</label>
+                <input type="password" placeholder="Password" />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+}
+export default Login;
