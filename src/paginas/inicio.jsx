fde6152682c43e42b3b1d9592803bd8b09f0ee7c
MaxStellMM <maximo.maroa@gmail.com>
2026-09-13 17:10:48 -0300

    Creando componente NavBar

--- src/paginas/inicio.jsx
+++ src/paginas/inicio.jsx
@@ -2,44 +2,19 @@
 function Inicio() {
   return (
       <div>
-        <link rel="stylesheet" href="../estilos/Inicio.css" />
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><hr class="dropdown-divider"/></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-          </li>
-        </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
-      </div>
+        <h1></h1>
+        <p></p>
+        <button><a href=""></a></button>
+        <button><a href=""></a></button>
+        <h2>Novedades</h2>
+        <div class="card" style="width: 18rem;">
+        <img src="" class="card-img-top" alt="" />
+          <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+          </div>
+        </div>
     </div>
-  </nav>
-    </div>
+
   )
 }
 export default Inicio
