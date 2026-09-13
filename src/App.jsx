f465af9255bdfd007c2d9c7220d009d047b81633
MaxStellMM <maximo.maroa@gmail.com>
2026-09-13 17:41:50 -0300

    Navbar prototipo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Inicio from "./paginas/Inicio";
 import Login from "./paginas/Login";
+import Navbar from "./componentes/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/paginas/Inicio" element={<Inicio />} />
--- src/componentes/Navbar.jsx
+++ src/componentes/Navbar.jsx
@@ -1,7 +1,26 @@
-function Navbar(){
-    return(
-    <div>
-        
-    </div>
-    )
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+            <Link className="navbar-brand" to="/">GagaSonicos</Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+                <li className="nav-item">
+                <Link className="nav-link" to="/">Inicio</Link>
+                </li>
+                <li className="nav-item">
+                <Link className="nav-link" to="/paginas/Login">Login</Link>
+                </li>
+            </ul>
+            </div>
+        </div>
+        </nav>
+    );
 }
+
+export default Navbar;
