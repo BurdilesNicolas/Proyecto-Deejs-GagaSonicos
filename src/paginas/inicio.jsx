335697a687a15ce3952bc5ce519e0ba436817fe6
MaxStellMM <maximo.maroa@gmail.com>
2026-09-18 20:31:49 -0300

    Borrando Carrusel

--- src/paginas/inicio.jsx
+++ src/paginas/inicio.jsx
@@ -2,17 +2,33 @@
 function Inicio() {
   return (
       <div>
-        <h1>Peldon</h1>
+        <h1>DEEJ`S</h1>
         <p></p>
         <button><a href=""></a></button>
         <button><a href=""></a></button>
         <h2>Novedades</h2>
         <div className="card" style={{ width: '18rem' }}>
+          <p>Tema 1</p>
         <img src="" className="card-img-top" alt="" />
           <div className="card-body">
           <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
           </div>
         </div>
+        <div className="card" style={{ width: '18rem' }}>
+          <p>Tema 2</p>
+        <img src="" className="card-img-top" alt="" />
+          <div className="card-body">
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+          </div>
+        </div>
+        <div className="card" style={{ width: '18rem' }}>
+          <p>Tema 3</p>
+        <img src="" className="card-img-top" alt="" />
+          <div className="card-body">
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+          </div>
+        </div>
+
     </div>
 
   )
