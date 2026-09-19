0ef5667af8bbd5b41d69e1821428f9220f816841
MaxStellMM <maximo.maroa@gmail.com>
2026-09-19 02:10:06 -0300

    Mejorando inicio.jsx

--- src/paginas/inicio.jsx
+++ src/paginas/inicio.jsx
@@ -35,6 +35,34 @@ function Inicio() {
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
                 </div>
               </div>
+              <div className="card" style={{ width: '18rem' }}>
+                <p>Tema 4</p>
+              <img src="" className="card-img-top" alt="" />
+                <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                </div>
+              </div>
+              <div className="card" style={{ width: '18rem' }}>
+                <p>Tema 5</p>
+              <img src="" className="card-img-top" alt="" />
+                <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                </div>
+              </div>
+              <div className="card" style={{ width: '18rem' }}>
+                <p>Tema 6</p>
+              <img src="" className="card-img-top" alt="" />
+                <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                </div>
+              </div>
+              <div className="card" style={{ width: '18rem' }}>
+                <p>Tema 7</p>
+              <img src="" className="card-img-top" alt="" />
+                <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                </div>
+              </div>
           </div>
         </div>
     </div>
