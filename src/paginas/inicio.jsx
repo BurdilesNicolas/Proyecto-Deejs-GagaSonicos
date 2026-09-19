e43764b970f985244e54d7de9cd8bc4a645855f3
MaxStellMM <maximo.maroa@gmail.com>
2026-09-18 22:51:08 -0300

    Mejorando pagina de inicio

--- src/paginas/inicio.jsx
+++ src/paginas/inicio.jsx
@@ -5,32 +5,35 @@ function Inicio() {
         <div className="hero">
         <h1>DEEJ`S</h1>
         <p></p>
-        <button><a href=""></a></button>
-        <button><a href=""></a></button>
+        <div className="botones_inicio">
+          <button><a href="..\paginas\Productos">Explorar</a></button>
+          <button><a href=""></a>Conocernos</button>
+        </div>
         </div>
         <h2>Novedades</h2>
-        <div className="card" style={{ width: '18rem' }}>
-          <p>Tema 1</p>
-        <img src="" className="card-img-top" alt="" />
-          <div className="card-body">
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-          </div>
+        <div className="cards">
+            <div className="card" style={{ width: '18rem' }}>
+              <p>Tema 1</p>
+            <img src="" className="card-img-top" alt="" />
+              <div className="card-body">
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+              </div>
+            </div>
+            <div className="card" style={{ width: '18rem' }}>
+              <p>Tema 2</p>
+            <img src="" className="card-img-top" alt="" />
+              <div className="card-body">
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+              </div>
+            </div>
+            <div className="card" style={{ width: '18rem' }}>
+              <p>Tema 3</p>
+            <img src="" className="card-img-top" alt="" />
+              <div className="card-body">
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+              </div>
+            </div>
         </div>
-        <div className="card" style={{ width: '18rem' }}>
-          <p>Tema 2</p>
-        <img src="" className="card-img-top" alt="" />
-          <div className="card-body">
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-          </div>
-        </div>
-        <div className="card" style={{ width: '18rem' }}>
-          <p>Tema 3</p>
-        <img src="" className="card-img-top" alt="" />
-          <div className="card-body">
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-          </div>
-        </div>
-
     </div>
 
   )
